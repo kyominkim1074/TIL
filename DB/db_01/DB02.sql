a2d4a9c84aeb411851a6600219f598cb92855a24
kyominkim1074 <kyominkim1074@gmail.com>
2022-08-18 13:29:08 +0900

    upload

--- DB/db_01/DB02.sql
+++ DB/db_01/DB02.sql
@@ -26,6 +26,14 @@ SELECT COUNT(*) FROM healthcare WHERE height>=160 AND height<=170;
 
 --5.
 SELECT * FROM healthcare WHERE is_drinking=1 AND waist !='' ORDER BY waist DESC LIMIT 5;
+SELECT * FROM healthcare WHERE is_drinking=1 AND waist <>'' ORDER BY waist DESC LIMIT 5;
+-- id      sido  gender  age  height  weight  waist  va_left  va_right  blood_pressure  smoking  is_drinking       
+-- ------  ----  ------  ---  ------  ------  -----  -------  --------  --------------  -------  -----------       
+-- 993531  48    1       9    170     130     146.0  0.6      0.8       150             3        1
+-- 87897   48    1       10   170     130     142.0  0.6      0.8       140             1        1
+-- 826643  48    1       9    180     135     141.4  1.2      0.9       136             3        1
+-- 567314  26    1       11   170     110     140.0  0.3      0.6       125             3        1
+-- 611146  36    1       12   165     120     140.0  0.4      0.8       141             3        1
 
 --6.
 SELECT COUNT(*) FROM healthcare WHERE va_left>=1.5 AND va_right>=1.5 AND is_drinking=1;
@@ -59,12 +67,13 @@ SELECT height, weight FROM healthcare ORDER BY height DESC LIMIT 1 OFFSET 2;
 
 --11.
 SELECT COUNT(*) FROM healthcare WHERE (weight/((height*height)*0.0001))>=30;
+SELECT COUNT(*) FROM healthcare WHERE weight/((height*0.01)*(height*0.01))>=30;
 -- COUNT(*)
 -- --------
 -- 53121
 
 --12.
-SELECT id, weight/((height*height)*0.0001) FROM healthcare WHERE smoking=3 ORDER BY (weight/((height*height)*0.0001)) DESC LIMIT 5;
+SELECT id, weight/((height*height)*0.0001) AS BMI FROM healthcare WHERE smoking=3 ORDER BY (weight/((height*height)*0.0001)) DESC LIMIT 5;
 -- id      weight/((height*height)*0.0001)
 -- ------  -------------------------------
 -- 231431  50.78125
