30f57b338e931fb42b106fa242a2f3e26f1b4a5f
kyominkim1074 <kyominkim1074@gmail.com>
2022-09-26 17:30:21 +0900

    0926

--- DJango/0922/0926_prac/practices/urls.py
+++ DJango/0922/0926_prac/practices/urls.py
@@ -1,4 +1,4 @@
-"""odd URL Configuration
+"""practices URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- DJango/0922/0926_prac/random_/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RandomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'random_'
