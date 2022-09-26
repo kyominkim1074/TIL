501361e0d150e3a047f00f5e1e4ba64a05cb6d25
kyominkim1074 <kyominkim1074@gmail.com>
2022-09-26 14:33:13 +0900

    0926

--- DJango/0922/numberrs/views.py
+++ DJango/0922/numberrs/views.py
@@ -1,3 +1,21 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def print_number(request, _number):
+    context = {
+        "number": _number,
+    }
+    return render(request, "number.html", context)
+
+
+def print_text(request):
+    text = request.GET.get("_text")
+    context = {
+        "template_text": text,
+    }
+    return render(request, "text.html", context)
--- DJango/0922/testing/urls.py
+++ DJango/0922/testing/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
+from numberrs import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.index),
+    path("number/<int:_number>", views.print_number),
+    path("print-text/", views.print_text),
 ]
