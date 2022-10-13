48e3c93bdb58e6ebfb51ad61e1063f8d57abe4a7
kyominkim1074 <kyominkim1074@gmail.com>
2022-10-13 22:50:25 +0900

    1013

--- DJango/1006-1011/movies/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-
-# Register your models here.
-class MovieAdmin(admin.ModelAdmin):
-    fields = ["title"]
-
-
-admin.site.register(Movie, MovieAdmin)
--- DJango/1006-1013/accounts/views.py
+++ DJango/1006-1013/accounts/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.shortcuts import redirect, render
 
 from accounts.models import User
 
 # from django.contrib.auth.forms import UserCreationForm
-from .forms import CustomUserCreationForm
-from django.contrib.auth import get_user_model
-from django.contrib.auth import login as auth_login
-from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 
