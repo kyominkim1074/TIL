b29512fcac03e5ca65f4af0dbb893a7772f2deaa
kyominkim1074 <kyominkim1074@gmail.com>
2022-10-24 10:51:38 +0900

    like_users

--- DJango/1018-1020/articles/models.py
+++ DJango/1018-1020/articles/models.py
@@ -16,6 +16,7 @@ class Article(models.Model):
                                 format='JPEG',
                                 options={'quality': 80})
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
 class Comment(models.Model):
     content = models.CharField(max_length=80)
