2e21b911547109f8daf32f87fade0cf38d98941d
kyominkim1074 <kyominkim1074@gmail.com>
2022-10-25 13:40:04 +0900

    1025

--- DJango/1018-1025/accounts/models.py
+++ DJango/1018-1025/accounts/models.py
@@ -8,3 +8,4 @@ class User(AbstractUser):
     @property
     def full_name(self):
         return f"{self.last_name}{self.first_name}"
+    followings = models.ManyToManyField(symmetrical=False, related_name='followers')
