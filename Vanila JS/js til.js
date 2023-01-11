b8ed276adeaa4137b55498abb768c0867e09e310
kyominkim1074 <kyominkim1074@gmail.com>
2023-01-11 21:43:11 +0900

    2023-01-11 JS

--- Vanila JS/js til.js
+++ Vanila JS/js til.js
@@ -1,49 +1,63 @@
-const num = 42;
-try {
-  num = 99;
-} catch (err) {
-  console.log(err);
-  // 오류 : TypeError: Assignment to constant variable.
-}
+// const num = 42;
+// try {
+//   num = 99;
+// } catch (err) {
+//   console.log(err);
+//   // 오류 : TypeError: Assignment to constant variable.
+// }
 
-console.log(num)
-// excepted output : 42
+// console.log(num)
+// // excepted output : 42
 
-let x = 1;
-if (x === 1) {
-  let x = 2;
-  console.log(x);
-  // excepted output : 2
-}
-console.log(x);
-// excepted output : 1
+// let x = 1;
+// if (x === 1) {
+//   let x = 2;
+//   console.log(x);
+//   // excepted output : 2
+// }
+// console.log(x);
+// // excepted output : 1
 
-var a = 1;
-if (a === 1) {
-  var a = 2;
-  console.log(a);
-  // excepted output : 2
-}
-console.log(a);
-// excepted output : 2
+// var a = 1;
+// if (a === 1) {
+//   var a = 2;
+//   console.log(a);
+//   // excepted output : 2
+// }
+// console.log(a);
+// // excepted output : 2
 
-var x_ = new Boolean(false);
-if (x_) {
-  // 이 코드는 실행됨
-}
+// var x_ = new Boolean(false);
+// if (x_) {
+//   // 이 코드는 실행됨
+// }
 
-var x__ = false;
-if (x__) {
-  // 이 코드는 실행되지 않음
-}
+// var x__ = false;
+// if (x__) {
+//   // 이 코드는 실행되지 않음
+// }
 
-var myFalse = new Boolean(false);   // 초기값 거짓
-var g = Boolean(myFalse);           // 초기값 참
-var myString = new String('Hello'); // 문자열 객체
-var s = Boolean(myString);          // 초기값 참
+// var myFalse = new Boolean(false);   // 초기값 거짓
+// var g = Boolean(myFalse);           // 초기값 참
+// var myString = new String('Hello'); // 문자열 객체
+// var s = Boolean(myString);          // 초기값 참
 
-let fruits = ['사과', '바나나']
-console.log(fruits.length)
-fruits.forEach(function (item, index, array) {
-  console.log(item, index)
-})
+// let fruits = ['사과', '바나나']
+// console.log(fruits.length)
+// fruits.forEach(function (item, index, array) {
+//   console.log(item, index)
+// })
+
+// const player = {
+//   name: "kim",
+//   points: 10,
+//   fat: false,
+// };
+
+// console.log(player);
+
+// player.fat = true;
+
+// console.log(player);
+// console.log(player.name);
+
