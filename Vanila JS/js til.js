13c4dc6c4264a3dabfb665c1fa58e6ddf8c54730
kyominkim1074 <kyominkim1074@gmail.com>
2023-01-20 17:32:17 +0900

    20230120

--- Vanila JS/js til.js
+++ Vanila JS/js til.js
@@ -111,11 +111,13 @@ const age = parseInt(prompt("age?"));
 if (isNaN(age) || age < 0) {
   console.log("Please write a real positive number.");
 } else if (age < 18) {
-  console.log("You are not adult.")
+  console.log("You are not adult.");
 } else if (age > 50 && age <= 50) {
-  console.log("You are adult.")
+  console.log("You are adult.");
 } else if (age > 50 && age <= 80) {
-  console.log("You should take care of your health.")
+  console.log("You should take care of your health.");
 } else if (age > 80) {
-  console.log("You can do whatever you want.")
+  console.log("You can do whatever you want.");
+} else if (age === 100) {
+  console.log("You are wise.");
 }
